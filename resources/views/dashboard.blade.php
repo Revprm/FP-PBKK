b9abae833fd5a3c187a3e7b0931bb85ec9c69db7
Revprm <revy.pramana@gmail.com>
2024-10-16 20:43:55 +0700

    fix: delete modal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -45,7 +45,8 @@
                                     <tbody class="bg-white divide-y divide-gray-200">
                                         @foreach ($products as $product)
                                             <tr>
-                                                <td class="px-6 py-4 whitespace-nowrap">{{ Str::words($product->name, 5) }}</td>
+                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                    {{ Str::words($product->name, 5) }}</td>
                                                 <td class="px-6 py-4 whitespace-nowrap">
                                                     ${{ number_format($product->price, 2) }}</td>
                                                 <td class="px-6 py-4 whitespace-nowrap">{{ $product->rate }}/10</td>
@@ -188,7 +189,7 @@
                                                         class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
                                                         <button type="button"
                                                             class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                                            data-modal-toggle="deleteProductModal">
+                                                            data-modal-toggle="deleteProductModal{{ $product->id }}">
                                                             <svg aria-hidden="true" class="w-5 h-5"
                                                                 fill="currentColor" viewBox="0 0 20 20"
                                                                 xmlns="http://www.w3.org/2000/svg">
@@ -198,18 +199,13 @@
                                                             </svg>
                                                             <span class="sr-only">Close modal</span>
                                                         </button>
-                                                        <svg class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto"
-                                                            aria-hidden="true" fill="currentColor"
-                                                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                            <path fill-rule="evenodd"
-                                                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                                clip-rule="evenodd"></path>
-                                                        </svg>
                                                         <p class="mb-4 text-gray-500 dark:text-gray-300">Are you sure
                                                             you want to
                                                             delete this item?</p>
                                                         <div class="flex justify-center items-center space-x-4">
-                                                            <button data-modal-toggle="deleteModal" type="button"
+                                                            <button
+                                                                data-modal-toggle="deleteProductModal{{ $product->id }}"
+                                                                type="button"
                                                                 class="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-green-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                                                                 No, cancel
                                                             </button>
